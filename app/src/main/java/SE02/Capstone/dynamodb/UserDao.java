c47087a5e7ace33694fe6c18513fdad1710e763e
Troy Prater <tprater93@gmail.com>
2023-05-22 11:27:58 -0500

    UserDao

--- app/src/main/java/SE02/Capstone/dynamodb/UserDao.java
+++ app/src/main/java/SE02/Capstone/dynamodb/UserDao.java
@@ -1,4 +1,33 @@
 package SE02.Capstone.dynamodb;
 
+import SE02.Capstone.dynamodb.models.User;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
 public class UserDao {
+
+    private final DynamoDBMapper dynamoDbMapper;
+
+    /**
+     * Instantiates an InventoryDao object.
+     *
+     * @param dynamoDbMapper the {@link DynamoDBMapper} used to interact with the album_track table
+     */
+
+    @Inject
+    public UserDao(DynamoDBMapper dynamoDbMapper) {
+        this.dynamoDbMapper = dynamoDbMapper;
+    }
+
+    public User getUser(String userId) {
+        User user = dynamoDbMapper.load(User.class, userId);
+        if (null == user) {
+            throw new NullPointerException();
+        }
+        return user;
+    }
+
 }
